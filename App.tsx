6446919d33b1b184123986f70cd0cdb3ac4f735c
Alysson Barrera <alyssonbarrera@outlook.com>
2023-02-14 14:13:58 -0300

    ✨ add new feature

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import { StatusBar } from 'react-native';
+import { StatusBar, Platform }  from 'react-native';
 import { NativeBaseProvider } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
@@ -6,15 +6,19 @@ import { Routes } from './src/routes';
 
 import { THEME } from './src/theme';
 import { Loading } from './src/components/Loading';
+import { tagUserEmailCreate } from './src/notifications/notificationsTags';
 
 import { CartContextProvider } from './src/contexts/CartContext';
 
 import OneSignal from 'react-native-onesignal';
 
-OneSignal.setAppId("90a690d4-e63e-4c7b-880d-3bd16133f167");
+const oneSignalAppId = Platform.OS === 'android' ? "90a690d4-e63e-4c7b-880d-3bd16133f167" : ""
+OneSignal.setAppId(oneSignalAppId);
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
+  tagUserEmailCreate('alyssonbarrera.s@gmail.com');
 
   return (
     <NativeBaseProvider theme={THEME}>
